961b8c99bd5b72481e951b4f1cdd831da4ab6fa3
Sergey Fominykh <FomS>
2023-01-27 17:02:22 +0300

    Version 2

--- lession_1/VariablesTheme.java
+++ lession_1/VariablesTheme.java
@@ -1,113 +1,97 @@
-public class VariablesTheme{    
+public class VariablesTheme {    
     public static void main(String[] args)  {
         System.out.println("\n1.Вывод значений переменных на консоль");
-        byte  core = 4;
-        short sizeram = 15;
-        int   countLogCpu = 8;
+        byte core = 4;
+        short sizeRam = 15;
+        int countLogCpu = 8;
         long countHdd = 4l;
         float freqCpu = 2675.4864f;
         double freqRam = 1066.7777777765d;
-        System.out.println("Ядер ЦП = " + core +"\nОбъём ОЗУ = " + sizeram + "\nКол.лог.ЦП = " 
+        char nameCpu = 'i';
+        boolean startComputer = true;
+        System.out.println("Ядер ЦП = " + core +"\nОбъём ОЗУ = " + sizeRam + "\nКол.лог.ЦП = " 
                 + countLogCpu + "\nКол.HDD = " + countHdd + "\nЧастота ЦП = " + freqCpu 
-                + "\nЧастота ОЗУ = " + freqRam);  
+                + "\nЧастота ОЗУ = " + freqRam + "Имя ЦП = " + nameCpu + "ПК включён? " + startComputer );  
 
         System.out.println("\n2.Расчет стоимости товара со скидкой");
-        int pricePen = 100;
-        int book = 200;
+        int penPrice = 100;
+        int bookPrice = 200;
         float discount = 0.11f;
-        float sumDiscount;
-        float sum;
-
-            // сумма скидки
-            sumDiscount  = ((pricePen + book)*discount);
-            // Итоговая сумма
-            sum = ((pricePen + book) - sumDiscount);
-        System.out.println("Скидка = " + sumDiscount + "\nИтого = " + sum);        
+        // сумма скидки
+        float discountSum = ((penPrice + bookPrice) * discount);
+        // Итоговая сумма
+        float discountPrice = ((penPrice + bookPrice) - discountSum );
+           
+        System.out.println("Скидка = " + discountSum  + "\nИтого = " + discountPrice);        
        
-        System.out.println("\n3.Вывод слова JAVA\n\n" + "   J    a  v     v  a" 
-                + "\n   J   a a  v   v  a a\n" + "J  J  aaaaa  V V  aaaaa\n" 
-                + " JJ  a     a  V  a     a" );
+        System.out.println("\n3.Вывод слова JAVA\n\n"); 
+        System.out.println("   J    a  v     v  a"+ "\n   J   a a  v   v  a a\n"+ 
+                "J  J  aaaaa  V V  aaaaa\n"+ " JJ  a     a  V  a     a" );
 
-        System.out.println("\n4.Вывод min и max значений целых числовых типов" );
+        System.out.println("\n4.Вывод min и max значений целых числовых типов");
 
-        byte  maxByte = 127;
+        byte maxByte = 127;
         short maxShort = 32767;
-        int   maxInt = 2147483647;
-        long  maxLong = 9223372036854775807l;
- 
+        int maxInt = 2147483647;
+        long maxLong = 9223372036854775807l; 
 
-        System.out.println("maxByte = " +  maxByte);
-        maxByte++;
-        System.out.println("maxByteI = " +  maxByte);
-        maxByte--;
-        System.out.println("maxByteD = " +  maxByte);
-
-        System.out.println("maxShort = " + maxShort);
-        maxShort++;
-        System.out.println("maxShortI = " + maxShort);
-        maxShort--;
-        System.out.println("maxShortD = " + maxShort);
-
-        System.out.println("maxInt = " + maxInt);
-        maxInt++;
-        System.out.println("maxIntI = " + maxInt);
-        maxInt--;
-        System.out.println("maxIntD = " + maxInt);
-
-        System.out.println("maxLong = " + maxLong);
-        maxLong++;
-        System.out.println("maxLongI = " + maxLong);
-        maxLong--;
-        System.out.println("maxLongD = " + maxLong);
+        System.out.println("maxByte = " + maxByte + "maxByteI = " + maxByte++ 
+                +"maxByteD = " + maxByte--);
+        System.out.println("maxShort = " + maxShort + "maxShortI = " + maxShort++ 
+                +"maxShortD = " + maxShort--);
+        System.out.println("maxInt = " + maxInt + "maxIntI = " + maxInt++ 
+                +"maxIntD = " + maxInt--);
+        System.out.println("maxLong = " + maxLong + "maxLongI = " + maxLong++ 
+                +"maxLongD = " + maxLong--);
 
         System.out.println("\n5.Перестановка значений переменных.");
         int a = 2;
         int b = 5;
-        int c;
+        int c = a;
         System.out.println("\ni.С помощью третьей переменной\nИсходное значение\na = " + a 
-                + "\nb = " + b);
-        c = a;
+                + "\nb = " + b);        
         a = b;
         b = c;
         System.out.println("\nРезультат\na = " + a + "\nb = " + b);
 
         System.out.println("\nii.С помощью арифметических операций\nИсходное значение\na = " 
                 + a + "\nb = " + b);
-        a = a + b;
+        a += b;
         b = a - b;
-        a = a - b;
+        a -= b;
         //a = a + b - (b = a); 
         System.out.println("Результат\na = " + a + "\nb = " + b);
 
         System.out.println("\niii.C помощью побитовой операции\nИсходное значение\na = " + a 
                 + "\nb = " + b);
-        a = a ^ b;
-        b = a ^ b;
-        a = a ^ b; 
+        a ^= b;
+        b ^= b;
+        a ^= b; 
         System.out.println("Результат\na = " + a + "\nb = " + b);
 
-        System.out.println("\n6.Вывод символов и их кодов" );
+        System.out.println("\n6.Вывод символов и их кодов");
         char num35 = '#';
         char num38 = '&';
         char num64 = '@';
         char num94 = '^';
         char num95 = '_';
-        System.out.println("Код = " + (int)num35 + " Символ = " +(char)num35);
-        System.out.println("Код = " + (int)num38 + " Символ = " + (char)num38 + "\nКод = " 
-                + (int)num64 + " Символ = " + (char)num64); 
-        System.out.println("Код = " + (int)num94 + " Символ = " + (char)num94 + "\nКод = " 
-                + (int)num95 + " Символ = " + (char)num95); 
+        System.out.println("Код = " + (int)num35 + " Символ = " + num35);
+        System.out.println("Код = " + (int)num38 + " Символ = " + num38 + 
+                "\nКод = " + (int)num64 + " Символ = " + num64); 
+        System.out.println("Код = " + (int)num94 + " Символ = " + num94 + 
+                "\nКод = " + (int)num95 + " Символ = " + num95); 
 
         System.out.println("\n7.Вывод в консоль ASCII-арт Дюка" );
-        char sl = '/';     
-        char osl = '\\';
-        char pod = '_';
-        char sk  = '(';
-        char osk = ')';        
-        System.out.println("    "+ sl + osl + "\n   " + sl + "  " + osl 
-                + "\n  " + sl + pod + sk + " " + osk + osl
-                + "\n " + sl + "      " + osl
-                + "\n" + sl + pod +pod +pod +pod + sl + osl + pod + pod + osl);
+        char slash = '/';     
+        char backSlash = '\\';
+        char underScore = '_';
+        char leftParentheses  = '(';
+        char rightParentheses = ')';        
+        System.out.println("    "+ slash + backSlash + "\n   " + slash + "  " + backSlash +
+                "\n  " + slash + underScore + leftParentheses + " " + rightParentheses + backSlash +
+                "\n " + slash + "      " + backSlash +
+                 "\n" + slash + underScore + underScore +underScore + underScore + slash +
+                backSlash + underScore + underScore + backSlash);
 
         System.out.println("\n8.Вывод количества сотен, десятков и единиц числа" );
         int n = 123;
